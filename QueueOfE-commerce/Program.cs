b38d9394e13bf9d8e88974b0dd5fe1ca643f368c
ZJXD <zhuhtd@foxmail.com>
2019-01-27 20:14:58 +0800

    秒杀
    一个用队列（Queue）实现的秒杀功能

---
+++ QueueOfE-commerce/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace QueueOfE_commerce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var thread = new ThreadPar();
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    string uid = "用户" + i;
+            //    RetData retData = thread.Buy(uid);
+            //    if (retData.Code == -1)
+            //    {
+            //        Console.WriteLine(uid + "：" + retData.Msg);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(uid + "：" + retData.Msg + "，还剩下" + retData.Cnt + "件。");
+            //    }
+            //}
+
+            Parallel.For(0, 100, (t, state) =>
+              {
+                  string uid = "用户" + t;
+                  RetData retData = thread.Buy(uid);
+                  if (retData.Code == -1)
+                  {
+                      Console.WriteLine(uid + "：" + retData.Msg);
+                  }
+                  else if (retData.Code == 1)
+                  {
+                      Console.WriteLine(uid + "：" + retData.Msg);
+                  }
+                  else
+                  {
+                      Console.WriteLine(uid + "：" + retData.Msg + "，还剩下" + retData.Cnt + "件。");
+                  }
+              });
+
+            Console.WriteLine("----------操作完成----------");
+            Console.ReadKey();
+        }
+    }
+}
